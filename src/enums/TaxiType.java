9de23f03254e94088269d8c541c65ef1ad9348d0
Jiidegul <emranlinigmail.com>
2023-01-05 14:28:55 +0300

    created taxi type enum

--- src/enums/TaxiType.java
+++ src/enums/TaxiType.java
@@ -1,5 +1,45 @@
 package enums;
 
-public enum TaxiType {
+import java.math.BigDecimal;
 
+public enum TaxiType {
+    STANDART(50,100),
+    COMFORT(60,200),
+    BUSINESS(80,300);
+
+    private BigDecimal pricePerKm;
+    
+   private BigDecimal priceForLanding;
+
+    TaxiType(BigDecimal pricePerKm, BigDecimal priceForLanding) {
+        this.pricePerKm = pricePerKm;
+        this.priceForLanding = priceForLanding;
+    }
+
+    TaxiType(int i, int i1) {
+    }
+
+    public BigDecimal getPricePerKm() {
+        return pricePerKm;
+    }
+
+    public void setPricePerKm(BigDecimal pricePerKm) {
+        this.pricePerKm = pricePerKm;
+    }
+
+    public BigDecimal getPriceForLanding() {
+        return priceForLanding;
+    }
+
+    public void setPriceForLanding(BigDecimal priceForLanding) {
+        this.priceForLanding = priceForLanding;
+    }
+
+    @Override
+    public String toString() {
+        return "TaxiType{" +
+                "pricePerKm=" + pricePerKm +
+                ", priceForLanding=" + priceForLanding +
+                '}';
+    }
 }
